0fa398e71aa6f4a47b257af93a62a1282fc8541e
Ben Sohanpal <bensohanpal2582@gmail.com>
2024-01-05 15:10:58 +0000

    Insert Vercel speed-insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
   import Portfolio from "../lib/components/Portfolio.svelte";
   import Footer from "$lib/components/Footer.svelte";
   import "../app.css";
+
+  import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
+
+  injectSpeedInsights();
 </script>
 
 <div class="bg-dark">
